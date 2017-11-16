651414615f07c76b76a761503fcba149b3fd83c8
Sam Douglas <sam.douglas32@gmail.com>
2017-11-17 09:20:08 +1300

    Add pushcsv mode to the client
    This needs more testing!!

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ import json
 import csv
 import requests
 import sys
+import usb.core
+import dateutil.parser
 
 import json_encoder
 
+@click.group()
+def cli():
+	pass
 
-@click.command()
+@cli.command()
 @click.option("--post-url", help="URL to post readings to")
 @click.option("--file", type=click.File(mode="w"), help="File to write output to")
 @click.option("--interval", default=10, help="Interval between readings")
-def cli(post_url, interval, file):
+def monitor(post_url, interval, file):
 	outputs = []
 	if post_url:
 		outputs.append(JSONHandler(post_url))
@@ -27,6 +32,19 @@ def cli(post_url, interval, file):
 		for output in outputs:
 			output(ts, temperature)
 
+@cli.command("pushcsv")
+@click.argument("input_file", type=click.File(mode="r"))
+@click.argument("post_url")
+def push_csv(input_file, post_url):
+	json_handler = JSONHandler(post_url, batch_size=None)
+	with input_file:
+		reader = csv.reader(input_file)
+		next(reader)
+		for row in reader:
+			ts = dateutil.parser.parse(row[0])
+			temp = float(row[1])
+			json_handler(ts, temp)
+		json_handler.flush()		
 
 class JSONHandler(object):
 	def __init__(self, url, batch_size=6):
@@ -44,23 +62,25 @@ class JSONHandler(object):
 	def _worker_task(self):
 		while True:
 			time.sleep(5)
-			try:
-				json_data = None
-				data_values_to_send = 0
-				with self._lock:
-					if len(self.data) >= self.batch_size:
-						data_values_to_send = len(self.data)
-						json_data = json.dumps(self.data, cls=json_encoder.JSONEncoder)
-				if json_data:
-					r = requests.post(self.url, data=json_data, headers={"Content-Type": "application/json"})
-					if r.status_code == requests.codes.ok:
-						# If this took a while there could be extra data values waiting 
-						with self._lock:
-							remaining_data = len(self.data) - data_values_to_send
-							self.data = self.data[:remaining_data]
-			except requests.exceptions.RequestException as e:
-				print(e, file=sys.stderr)
+			if self.batch_size and len(self.data) >= self.batch_size:
+				self.flush()
 
+	def flush(self):
+		try:
+			json_data = None
+			data_values_to_send = 0
+			with self._lock:
+				data_values_to_send = len(self.data)
+				json_data = json.dumps(self.data, cls=json_encoder.JSONEncoder)
+			if json_data:
+				r = requests.post(self.url, data=json_data, headers={"Content-Type": "application/json"})
+				if r.status_code == requests.codes.ok:
+					# If this took a while there could be extra data values waiting
+					with self._lock:
+						remaining_data = len(self.data) - data_values_to_send
+						self.data = self.data[:remaining_data]
+		except requests.exceptions.RequestException as e:
+			print(e, file=sys.stderr)
 
 	def __call__(self, timestamp, temperature):
 		self._start_worker()
@@ -81,8 +101,12 @@ class FileHandler(object):
 
 def poll_temperatures(interval):
 	while True:
-		yield get_data_point()
-		time.sleep(interval)
+		try:
+			yield get_data_point()
+		except usb.core.USBError as e:
+			print("USB Error: " + str(e), file=sys.stderr)
+		finally:
+			time.sleep(interval)
 
 
 def get_temperature():
